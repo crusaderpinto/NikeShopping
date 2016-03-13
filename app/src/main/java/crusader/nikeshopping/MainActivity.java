d8c37468b7f9c059066c5b8a5b86bbd2777a85c8
CRUSADER1912 <saldanha.linus@gmail.com>
2016-03-13 21:10:30 +0530

    Added login and search

--- app/src/main/java/crusader/nikeshopping/MainActivity.java
+++ app/src/main/java/crusader/nikeshopping/MainActivity.java
@@ -1,5 +1,6 @@
 package crusader.nikeshopping;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,10 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 import com.squareup.okhttp.OkHttpClient;
@@ -19,7 +23,9 @@ import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.List;
 
+import crusader.nikeshopping.adapters.EndlessRecyclerViewAdapter;
 import crusader.nikeshopping.adapters.MyAdapter;
 import crusader.nikeshopping.models.RetriveByKeyWord;
 import retrofit.Call;
@@ -28,10 +34,20 @@ import retrofit.GsonConverterFactory;
 import retrofit.Response;
 import retrofit.Retrofit;
 
-public class MainActivity extends AppCompatActivity implements Callback<RetriveByKeyWord> {
+public class MainActivity extends AppCompatActivity implements Callback<RetriveByKeyWord>, EndlessRecyclerViewAdapter.RequestToLoadMoreListener, View.OnClickListener {
     RecyclerView rv_prodPreview;
     private MyAdapter mAdapter;
     RetriveByKeyWord myDataset;
+
+    int currentPageCount = 0;
+    int totalPageCount = 0;
+
+    EditText edtSearch;
+    Button btnSearch;
+
+    String searchTerm = "";
+
+    private EndlessRecyclerViewAdapter endlessRecyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +67,21 @@ public class MainActivity extends AppCompatActivity implements Callback<RetriveB
 
         initComponents();
 
-        makeApiCall();
+        makeApiCall(searchTerm);
     }
 
     private void initComponents() {
         rv_prodPreview = (RecyclerView) findViewById(R.id.rv_detail_list);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv_prodPreview.setLayoutManager(llm);
+
+        edtSearch = (EditText) findViewById(R.id.edt_search_term);
+        btnSearch = (Button) findViewById(R.id.btn_search);
+
+        btnSearch.setOnClickListener(this);
     }
 
-    private void makeApiCall() {
+    private void makeApiCall(String searchTerm) {
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
 // set your desired log level
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -76,11 +97,11 @@ public class MainActivity extends AppCompatActivity implements Callback<RetriveB
                 .client(httpClient)
                 .build();
         RetroApi api = retrofit.create(RetroApi.class);
-        Call<RetriveByKeyWord> call = api.getProducts(getDataMap());
+        Call<RetriveByKeyWord> call = api.getProducts(getDataMap(searchTerm));
         call.enqueue(this);
     }
 
-    private HashMap getDataMap() {
+    private HashMap getDataMap(String searchTerm) {
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("OPERATION-NAME", "findItemsByKeywords");
         map.put("SERVICE-VERSION", "1.0.0");
@@ -89,11 +110,12 @@ public class MainActivity extends AppCompatActivity implements Callback<RetriveB
         map.put("RESPONSE-DATA-FORMAT", "JSON");
         map.put("REST-PAYLOAD", "");
         try {
-            map.put("keywords", URLEncoder.encode("Nike","utf-8"));
+            map.put("keywords", URLEncoder.encode("Nike " + searchTerm, "utf-8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         map.put("paginationInput.entriesPerPage", "10");
+        map.put("paginationInput.pageNumber", String.valueOf(currentPageCount + 1));
         return map;
     }
 
@@ -122,22 +144,80 @@ public class MainActivity extends AppCompatActivity implements Callback<RetriveB
     @Override
     public void onResponse(Response<RetriveByKeyWord> response, Retrofit retrofit) {
         myDataset = response.body();
-        mAdapter = new MyAdapter(this, myDataset.getFindItemsByKeywordsResponse().get(0).getSearchResult());
-        rv_prodPreview.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new MyAdapter(this, myDataset.getFindItemsByKeywordsResponse().get(0).getSearchResult());
 
-        /**Endless ADapter**/
-//        endlessRecyclerViewAdapter = new EndlessRecyclerViewAdapter(this, mAdapter, this);
-//        rv_prodPreview.setAdapter(endlessRecyclerViewAdapter);
-        /********************/
+            /**Endless ADapter**/
+            endlessRecyclerViewAdapter = new EndlessRecyclerViewAdapter(this, mAdapter, this);
+            rv_prodPreview.setAdapter(endlessRecyclerViewAdapter);
+            /********************/
+        }
+        totalPageCount = Integer.valueOf(myDataset.getFindItemsByKeywordsResponse().get(0).getPaginationOutput().get(0).getTotalPages().get(0));
+        currentPageCount = Integer.valueOf(myDataset.getFindItemsByKeywordsResponse().get(0).getPaginationOutput().get(0).getPageNumber().get(0));
+
+        if (currentPageCount >= totalPageCount) {
+            endlessRecyclerViewAdapter.onDataReady(false);
+        } else {
+            // notify the data is ready
+            endlessRecyclerViewAdapter.onDataReady(true);
+        }
 
         String responsetxt = new Gson().toJson(response);
-//        Log.d("CRUSADER", responsetxt);
-        Utility.showLog(responsetxt);
+        Log.d("CRUSADER", responsetxt);
     }
 
     @Override
     public void onFailure(Throwable t) {
-        Log.d("CRUSADER", t.getMessage());
+        Log.d("CRUSADER", "" + t.getMessage());
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_search:
+                if (!TextUtils.isEmpty(edtSearch.getText().toString())) {
+                    searchTerm = edtSearch.getText().toString();
+                    resetPageCounter();
+                    makeApiCall(searchTerm);
+                } else {
+                    edtSearch.setError(getString(R.string.txt_empty_search_term));
+                }
+                break;
+        }
+    }
+
+    private void resetPageCounter() {
+        currentPageCount = 0;
+        totalPageCount = 0;
+        mAdapter = null;
+    }
+
+    @Override
+    public void onLoadMoreRequested() {
+        new AsyncTask<Void, Void, List>() {
+            @Override
+            protected List doInBackground(Void... params) {
+//                try {
+//                    Thread.sleep(200);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List list) {
+                if (currentPageCount > 1) {
+                    mAdapter.appendItems(myDataset.getFindItemsByKeywordsResponse().get(0).getSearchResult());
+                }
+                makeApiCall(searchTerm);
+//                if(currentPageCount >= totalPageCount){
+//                    endlessRecyclerViewAdapter.onDataReady(false);
+//                }else{
+//                    // notify the data is ready
+//                    endlessRecyclerViewAdapter.onDataReady(true);
+//                }
+            }
+        }.execute();
+    }
 }
